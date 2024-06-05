a7b8adb691434c1518b6c55bcd3f63a80b45de19
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-06-05 20:50:04 -0300

    assets

--- cadastro-usuarios/src/pages/Home/index.jsx
+++ cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,9 +1,25 @@
 import "./style.css";
+import Trash from "../../assets/trash.svg";
 
 function Home() {
   return (
-    <div>
-      <h1>OLÁ REACT</h1>
+    <div className="container">
+      <form>
+        <h1>Cadastro de Usuários</h1>
+        <input name="nome" type="text" />
+        <input name="idade" type="number" />
+        <input name="email" type="email" />
+        <button type="button">Cadastrar</button>
+      </form>
+
+      <div>
+        <div>
+          <p>Nome: </p>
+          <p>Idade: </p>
+          <p>Email: </p>
+        </div>
+        <button src={Trash}></button>
+      </div>
     </div>
   );
 }
