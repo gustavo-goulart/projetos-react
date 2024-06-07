624ed48bc97988c19e26e4c788478265915449b1
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-06-07 15:55:16 -0300

    front end

--- cadastro-usuarios/src/pages/Home/index.jsx
+++ cadastro-usuarios/src/pages/Home/index.jsx
@@ -1,25 +1,55 @@
 import "./style.css";
-import Trash from "../../assets/trash.svg";
+import Lixeira from "../../assets/trash.svg";
 
 function Home() {
+  const users = [
+    {
+      id: "2145usghsgfa",
+      name: "Gustavo",
+      age: 42,
+      email: "gustavo@gmail.com",
+    },
+
+    {
+      id: "2145usgrtay",
+      name: "Denise",
+      age: 35,
+      email: "denise@gmail.com",
+    },
+
+    {
+      id: "2145usg",
+      name: "Theo",
+      age: 9,
+      email: "theo@gmail.com",
+    },
+  ];
   return (
     <div className="container">
       <form>
         <h1>Cadastro de Usuários</h1>
-        <input name="nome" type="text" />
-        <input name="idade" type="number" />
-        <input name="email" type="email" />
+        <input name="nome" type="text" placeholder="Nome" />
+        <input name="idade" type="number" placeholder="Idade" />
+        <input name="email" type="email" placeholder="Email" />
         <button type="button">Cadastrar</button>
       </form>
 
-      <div>
-        <div>
-          <p>Nome: </p>
-          <p>Idade: </p>
-          <p>Email: </p>
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>
+              Nome: <span>{user.name}</span>{" "}
+            </p>
+            <p>
+              Idade: <span>{user.age}</span>{" "}
+            </p>
+            <p>
+              Email: <span>{user.email}</span>{" "}
+            </p>
+          </div>
+          <button src={Lixeira}></button>
         </div>
-        <button src={Trash}></button>
-      </div>
+      ))}
     </div>
   );
 }
